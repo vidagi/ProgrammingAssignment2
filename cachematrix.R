99a441816cf200ca52eab77919edfe1a75379d02
vidagi <vdagilis@hotmail.com>
2015-04-26 13:43:10 +0200

    v005
    some additional comments, doesn't print the message about usage of the
    cache - just works

--- cachematrix.R
+++ cachematrix.R
@@ -3,29 +3,29 @@
 
 ## Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL                             ##while being described, makes the cache empty
-        set <- function(y) {                    ##describes the 'set' method
+        inv <- NULL                             ## while being described, makes the cache empty
+        set <- function(y) {                    ## describes the 'set' method
                 x <<- y                         ##      asigns the matrix
                 inv <<- NULL                    ##      makes the cached inverse empty
         }
-        get <- function() x                     ##describes the 'get' method
-        setinv <- function(inv) inv <<- inv     ##describes 'setinv' method
-        getinv <- function() inv                ##describes 'getinv' method
-        list(set = set, get = get,
+        get <- function() x                     ## describes the 'get' method for the part 'x'
+        setinv <- function(inv) inv <<- inv     ## describes 'setinv' method for the cached value
+        getinv <- function() inv                ## describes 'getinv' method for the cached value
+        list(set = set, get = get,              ## makes a list with corresponding names
              setinv = setinv,
              getinv = getinv)
 }
 
 ## instead of solving every time, first checks if there is stored value in the cache
 cacheSolve <- function(x, ...) {
-        inv <- x$getinv()                     ##first tries to take cashed inversed matrix
-        if(!is.null(inv)) {                   ##if it contains smthg 
-                message("getting cached data")  
-                return(inv)                   ##brings as final inversed matrix from cache
+        inv <- x$getinv()                     ## first tries to take the cashed value of the inversed matrix
+        if(!is.null(inv)) {                   ## if it contains smthg 
+  #             message("getting cached data")  ##announces usage of the cache - has not been specified in the assignment, was used by me for the debugging
+                return(inv)                   ## brings as final inversed matrix from cache
         }                                     ## if not
-        data <- x$get()                       ##takes the normal data of the matrix
+        data <- x$get()                       ## takes the normal data of the matrix
         inv <- solve(data, ...)               ## solves, e.g. finds the inverse matrix
-        x$setinv(inv)                         ## writes to the cashe
-        inv                                   ## returns the the calculated inverse matrix
+        x$setinv(inv)                         ## writes it to the cache
+        inv                                   ## and returns the the calculated inverse matrix
         ## Return a matrix that is the inverse of 'x'
 }
